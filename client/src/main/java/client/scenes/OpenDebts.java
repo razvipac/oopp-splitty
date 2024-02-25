4ba15244711ebf5d40205ce99ed3629f618bc3e4
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-02-25 15:04:29 +0100

    Draft version of OpenDebts.java

--- client/src/main/java/client/scenes/OpenDebts.java
+++ client/src/main/java/client/scenes/OpenDebts.java
@@ -18,6 +18,9 @@ public class OpenDebts {
     public Scene scene;
     private ArrayList<Debt> debtList;
 
+    /**
+     * Constructor for Open Debts page
+     */
     public OpenDebts() {
         // Data for testing purposes
         Person john = new Person("John", "Doe");
@@ -32,25 +35,88 @@ public class OpenDebts {
         createScene();
     }
 
+    /**
+     * Creates the scene
+     */
     public void createScene() {
-        // Header
-        Text header = new Text("Open Debts");
-
-        HBox debtItem = new HBox(5);
-        Debt first = debtList.get(1);
-        String itemString = first.getDebtor().firstName + " gives "
-                + first.getAmount() + " Euro to " + first.getCreditor().firstName;
-        Label itemLabel = new Label(itemString);
-        debtItem.getChildren().addAll(itemLabel);
-
         // Layout
         VBox layout = new VBox();
         layout.setPadding(new Insets(10));
-        // adding elements to layout
-        layout.getChildren().addAll(header, debtItem);
+
+        // Header
+        Text header = new Text("Open Debts");
+        header.setFont(Font.font("Arial", FontWeight.BOLD , 20));
+        layout.getChildren().add(header);
+
+        // Adding each debt
+        for(Debt d : debtList) {
+            addDebtToLayout(d, layout);
+        }
 
         // Scene
-        scene = new Scene(layout, 270, 200);
+        scene = new Scene(layout, 400, 400);
+    }
+
+    /**
+     * Adds the specified debt to the layout
+     * @param d Debt to be added
+     * @param layout Layout to add it to
+     */
+    private static void addDebtToLayout(Debt d, VBox layout) {
+        // debtLine: line containing debtString and 'Mark Received' button
+        HBox debtLine = new HBox(5);
+        // debtItem: the entire debt item, containing the debtLine and debtInfo
+        VBox debtItem = new VBox(5);
+
+        // debtString & debtStringLabel, contains the debtor, creditor and amount
+        String debtorName = d.getDebtor().firstName;
+        String creditorName = d.getCreditor().firstName;
+        double amount = d.getAmount();
+        String debtString = debtorName + " gives " + amount + " Euro to " + creditorName;
+        Label debtStringLabel = new Label(debtString);
+
+        // 'Mark Received' button. Prints effect to console for testing
+        Button receivedButton = new Button("Mark received");
+        receivedButton.setOnAction(event -> {
+            d.setReceived(!d.isReceived());
+            if(d.isReceived()) {
+                receivedButton.setText("Undo");
+                System.out.println("The debt (" + debtString + ") is marked as received");
+            }
+            else {
+                receivedButton.setText("Mark received");
+                System.out.println("The debt (" + debtString + ") is marked as not received");
+            }
+        });
+
+        // extra debt info (bank information)
+        VBox debtInfo = new VBox(5);
+        Text bankInfo = new Text("Bank information available, transfer money to:\n" +
+                "Account Holder: John Doe\n" +
+                "IBAN: NL12 3456 7890 1234 56\n" +
+                "BIC: ABCDEFGH");
+        debtInfo.getChildren().addAll(bankInfo);
+        debtInfo.setVisible(false);
+        debtInfo.setManaged(false);
+
+        // toggle button for debtInfo, can be shown or hidden
+        ToggleButton moreInfo = new ToggleButton(">");
+        moreInfo.setOnAction(event -> {
+            if(moreInfo.isSelected()) {
+                moreInfo.setText("v");
+                debtInfo.setVisible(true);
+                debtInfo.setManaged(true);
+            }
+            else {
+                moreInfo.setText(">");
+                debtInfo.setVisible(false);
+                debtInfo.setManaged(false);
+            }
+        });
+
+        debtLine.getChildren().addAll(moreInfo, debtStringLabel, receivedButton);
+        debtItem.getChildren().addAll(debtLine, debtInfo);
+        layout.getChildren().add(debtItem);
     }
 
 }
