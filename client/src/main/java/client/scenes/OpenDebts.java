36cc74cb7cced9c0cf9b9cd2eb28f9be04dfa2fa
Naman Choudhary <namanchoudhary@student.tudelft.nl>
2024-02-25 16:37:07 +0100

    set Scene private OpenDebts class and also fixed 3 warnings Intellij (objects could be final and String concatenation can be replaced with a text block)

--- client/src/main/java/client/scenes/OpenDebts.java
+++ client/src/main/java/client/scenes/OpenDebts.java
@@ -99,10 +99,11 @@ public class OpenDebts {
 
         // extra debt info (bank information)
         VBox debtInfo = new VBox(5);
-        Text bankInfo = new Text("Bank information available, transfer money to:\n" +
-                "Account Holder: John Doe\n" +
-                "IBAN: NL12 3456 7890 1234 56\n" +
-                "BIC: ABCDEFGH");
+        Text bankInfo = new Text("""
+                Bank information available, transfer money to:
+                Account Holder: John Doe
+                IBAN: NL12 3456 7890 1234 56
+                BIC: ABCDEFGH""");
         debtInfo.setPadding(new Insets(0, 0, 10, 0));
         debtInfo.getChildren().addAll(bankInfo);
         debtInfo.setVisible(false);
