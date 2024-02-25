053710d8f388e02ae549457f2661f9a6219a6c4a
Stanisław Malinowski <s.p.malinowski@student.tudelft.pl>
2024-02-25 16:11:27 +0100

    Add JavaDoc

--- server/src/main/java/server/api/ExpenseBody.java
+++ server/src/main/java/server/api/ExpenseBody.java
@@ -1,5 +1,12 @@
 package server.api;
 
+/**
+ * Structure of the request body for Create/Update operations on expenses
+ *
+ * @param price price in euros
+ * @param item item for which a person paid
+ * @param participantId id of the participant who paid
+ */
 public record ExpenseBody(
         Integer price,
         String item,
--- server/src/main/java/server/service/ExpenseService.java
+++ server/src/main/java/server/service/ExpenseService.java
@@ -19,12 +19,24 @@ public class ExpenseService {
         this.expenseRepository = expenseRepository;
     }
 
+    /**
+     * Fetches a list of all expense objects in the datasource
+     * @return list of all expense objects in the datasource
+     */
     public List<Expense> getAll(){
         List<Expense> result = new LinkedList<>();
         expenseRepository.findAll().iterator().forEachRemaining(result::add);
         return result;
     }
 
+    /**
+     * Fetches one expense object from the datasource by it's ID
+     *
+     * @param id id of the object to be fetched
+     * @return the fetched expense object
+     * @throws NotFoundInDatabaseException
+     * if the object with the passed ID does not exist in the datasource
+     */
     public Expense getOneById(Long id) throws NotFoundInDatabaseException {
         Optional<Expense> search = expenseRepository.findById(id);
 
@@ -34,6 +46,13 @@ public class ExpenseService {
         throw new NotFoundInDatabaseException("Expense was not found in the database");
     }
 
+    /**
+     * Adds an expense object with data specified in the passed ExpenseBody to
+     * the datasource
+     *
+     * @param body data to populate the new Expense instance with
+     * @return the Expense object that got created
+     */
     public Expense create(ExpenseBody body){
         // TODO: fetch participant
 
@@ -46,6 +65,14 @@ public class ExpenseService {
         return newExpense;
     }
 
+    /**
+     * Deletes an Expense object with the passed ID from the datasource
+     *
+     * @param id ID of the object to be deleted
+     * @return the Expense object that got deleted
+     * @throws NotFoundInDatabaseException
+     * if a object with the passed ID does not exist in the datasource
+     */
     public Expense deleteById(Long id) throws NotFoundInDatabaseException {
         Expense found = getOneById(id);
         // if not found exception will be thrown
@@ -53,6 +80,15 @@ public class ExpenseService {
         return found;
     }
 
+    /**
+     * Modifies the data of an object with a passed ID inside the datasource
+     *
+     * @param id ID of the object to be modified
+     * @param body ExpenseBody object specifying the new state of the modified object
+     * @return the modified Expense object
+     * @throws NotFoundInDatabaseException
+     * if an object with passed ID does not exist in the datasource
+     */
     public Expense updateById(Long id, ExpenseBody body) throws NotFoundInDatabaseException {
         Expense found = getOneById(id);
         // if not found exception will be thrown
