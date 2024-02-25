f0a9adaeaf7e2b22d262b02b2b88e9d3e68e12d2
Razvan Paraschiv <r.n.paraschiv@student.tudelft.nl>
2024-02-25 12:37:06 +0100

    Classes Event and Date are finished

--- commons/src/main/java/commons/Date.java
+++ commons/src/main/java/commons/Date.java
@@ -1,6 +1,109 @@
 package commons;
 
+import java.util.Objects;
+
 public class Date {
 
-    
+    private int day;
+    private String month;
+    private int year;
+
+    /**
+     * Initializing an empty Date.
+     */
+    public Date() {
+    }
+
+    /**
+     * Initializing a Date with proper attributes
+     *
+     * @param day   The day of the respective date
+     * @param month The month of the respective date
+     * @param year  The year of the respective date
+     */
+    public Date(int day, String month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    /**
+     * @return the day of an instance of type Date
+     */
+    public int getDay() {
+        return day;
+    }
+
+    /**
+     * @return the month of an instance of type Date
+     */
+    public String getMonth() {
+        return month;
+    }
+
+    /**
+     * @return the year of an instance of type Date
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * Changing the value of the day
+     *
+     * @param day The day of a date
+     */
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    /**
+     * Changing the value of the month
+     *
+     * @param month The month of a date
+     */
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    /**
+     * Changing the value of the year
+     *
+     * @param year The year of a date
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    /**
+     * A proper equals method for the class Date
+     *
+     * @param o another object with which we compare
+     * @return whether the two objects are equal
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Date date = (Date) o;
+        return day == date.day && year == date.year && month.equals(date.month);
+    }
+
+    /**
+     * A proper hashCode for the class Date
+     *
+     * @return the hashCode of an instance of this class
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(day, month, year);
+    }
+
+    /**
+     * @return the format in which the dates shall appear
+     */
+    @Override
+    public String toString() {
+        return day + "-" + month + "-" + year;
+    }
 }
--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -9,9 +9,20 @@ public class Event {
     private Date creationDate;
     private Date lastActivity;
 
+    /**
+     * Initializing an empty Event
+     */
     public Event() {
     }
 
+    /**
+     * Initializing an Event with proper attributes
+     *
+     * @param name         The name of the respective event
+     * @param code         The code of the respective event
+     * @param creationDate The creation date of the respective event
+     * @param lastActivity The last activity of the respective event
+     */
     public Event(String name, int code, Date creationDate, Date lastActivity) {
         this.name = name;
         this.code = code;
@@ -19,38 +30,76 @@ public class Event {
         this.lastActivity = lastActivity;
     }
 
+    /**
+     * @return the name of an instance of type Event
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return the code of an instance of type Event
+     */
     public int getCode() {
         return code;
     }
 
+    /**
+     * @return the creation date of an instance of type Event
+     */
     public Date getCreationDate() {
         return creationDate;
     }
 
+    /**
+     * @return the last activity of an instance of type Event
+     */
     public Date getLastActivity() {
         return lastActivity;
     }
 
+    /**
+     * Changing the value of the name
+     *
+     * @param name The name of an event
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Changing the value of the code
+     *
+     * @param code The code of an event
+     */
     public void setCode(int code) {
         this.code = code;
     }
 
+    /**
+     * Changing the value of the creation date
+     *
+     * @param creationDate The creation date of an event
+     */
     public void setCreationDate(Date creationDate) {
         this.creationDate = creationDate;
     }
 
+    /**
+     * Changing the value of the last activity
+     *
+     * @param lastActivity The last activity of an event
+     */
     public void setLastActivity(Date lastActivity) {
         this.lastActivity = lastActivity;
     }
 
+    /**
+     * A proper equals method for the class Event
+     *
+     * @param o another object with which we compare
+     * @return whether the two objects are equal
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -61,16 +110,24 @@ public class Event {
         return code == event.code && name.equals(event.name) && creationDate.equals(event.creationDate) && lastActivity.equals(event.lastActivity);
     }
 
+    /**
+     * A proper hashCode for the class Event
+     *
+     * @return the hashCode of an instance of this class
+     */
     @Override
     public int hashCode() {
         return Objects.hash(name, code, creationDate, lastActivity);
     }
 
+    /**
+     * @return the format in which the events shall appear
+     */
     @Override
     public String toString() {
         return "Event " + name + ":" +
                 "\t- code = " + code +
-                "\t- creationDate = " + creationDate +
-                "\t- lastActivity = " + lastActivity;
+                "\t- creationDate = " + creationDate.toString() +
+                "\t- lastActivity = " + lastActivity.toString();
     }
 }
