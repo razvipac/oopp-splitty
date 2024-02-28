0853191955b648ace37ae4ab077ec81f5dc4f9f3
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-02-28 21:33:48 +0100

    Add header and non-functional Send Invite button in EventOverview

--- client/src/main/java/client/scenes/EventOverview.java
+++ client/src/main/java/client/scenes/EventOverview.java
@@ -1,10 +1,10 @@
 package client.scenes;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-//import javafx.geometry.Insets;
-//import javafx.scene.control.*;
-//import javafx.scene.layout.*;
-//import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
 
 public class EventOverview {
 
@@ -21,7 +21,25 @@ public class EventOverview {
      * Creates the scene
      */
     public void createScene() {
+        // Layout
+        VBox layout = new VBox();
+        layout.setPadding(new Insets(10));
 
+        // HBox with Header and Send Invite button
+        HBox boxTop = new HBox(5);
+
+        // Header
+        Text header = new Text("Header");
+        header.setFont(Font.font("Arial", FontWeight.BOLD , 20));
+        // Send Invite Button
+        Button sendInviteButton = new Button("Send Invite");
+        // Add to boxTop
+        boxTop.getChildren().addAll(header, sendInviteButton);
+
+        layout.getChildren().add(boxTop);
+
+        // Scene
+        scene = new Scene(layout, 400, 400);
     }
 
     /**
