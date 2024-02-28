52dfc312f32d9e793c894bc631e657d52df3118c
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-02-28 20:42:13 +0100

    Add EventOverview class for Event Overview page GUI

---
+++ client/src/main/java/client/scenes/EventOverview.java
@@ -0,0 +1,34 @@
+package client.scenes;
+
+import javafx.scene.Scene;
+//import javafx.geometry.Insets;
+//import javafx.scene.control.*;
+//import javafx.scene.layout.*;
+//import javafx.scene.text.*;
+
+public class EventOverview {
+
+    private Scene scene;
+
+    /**
+     * Constructor for Event Overview page
+     */
+    public EventOverview() {
+        createScene();
+    }
+
+    /**
+     * Creates the scene
+     */
+    public void createScene() {
+
+    }
+
+    /**
+     * Getter for the scene
+     * @return the scene
+     */
+    public Scene getScene() {
+        return scene;
+    }
+}
