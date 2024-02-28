a3a23b75d8069f9d173715dcecb0eed326970c5d
Stanisław Malinowski <s.p.malinowski@student.tudelft.nl>
2024-02-28 22:02:07 +0100

    Adapt tests to changes

--- commons/src/test/java/commons/ParticipantTest.java
+++ commons/src/test/java/commons/ParticipantTest.java
@@ -1,11 +1,44 @@
 package commons;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 class ParticipantTest {
-    Participant p1 = new Participant("A", "1@mail", "1234", "4321");
-    Participant p2 = new Participant("A", "1@mail", "1234", "4321");
-    Participant p3 = new Participant("V", "1@mail", "1234", "4321");
+    Participant p1 = new Participant(
+            "A",
+            new Event(
+                    "test",
+                    "codecode",
+                    LocalDateTime.of(1900, 1, 1, 0, 0, 0)
+                    ),
+            "a@mail.com",
+            "1234",
+            "1234"
+    );
+
+    Participant p2 = new Participant(
+            "A",
+            new Event(
+                    "test",
+                    "codecode",
+                    LocalDateTime.of(1900, 1, 1, 0, 0, 0)
+            ),
+            "a@mail.com",
+            "1234",
+            "1234"
+    );
+    Participant p3 = new Participant(
+            "V",
+            new Event(
+                    "test",
+                    "codecode",
+                    LocalDateTime.of(1900, 1, 1, 0, 0, 0)
+            ),
+            "a@mail.com",
+            "1234",
+            "1234"
+    );
 
     @Test
     void getName() {
@@ -16,17 +49,20 @@ class ParticipantTest {
     void setName() {
         assertNotEquals(p1, p3);
         p3.setName("A");
+        p1.setEvent(p3.getEvent());
         assertEquals(p1, p3);
     }
     @Test
     void testEquals() {
-        assertTrue(p1.equals(p2));
-        assertTrue(p1.equals(p1));
-        assertFalse(p1.equals(p3));
+        p1.setEvent(p2.getEvent());
+        assertEquals(p1, p2);
+        assertEquals(p1, p1);
+        assertNotEquals(p1, p3);
     }
 
     @Test
     void testHashCode() {
+        p1.setEvent(p2.getEvent());
         assertEquals(p1.hashCode(), p2.hashCode());
     }
 }
