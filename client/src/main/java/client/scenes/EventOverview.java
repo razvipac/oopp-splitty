4d3194a416b9a28eadaf9a9569694bf6f078747d
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-02-28 22:19:57 +0100

    Add text with event name and participants (hardcoded) Send Invite, Add and Edit Participant buttons.

--- client/src/main/java/client/scenes/EventOverview.java
+++ client/src/main/java/client/scenes/EventOverview.java
@@ -9,6 +9,8 @@ import javafx.scene.text.*;
 public class EventOverview {
 
     private Scene scene;
+    private final Font h1 = Font.font("Arial", FontWeight.BOLD , 20);
+    private final Font h2 = Font.font("Arial", FontWeight.BOLD , 14);
 
     /**
      * Constructor for Event Overview page
@@ -25,18 +27,32 @@ public class EventOverview {
         VBox layout = new VBox();
         layout.setPadding(new Insets(10));
 
-        // HBox with Header and Send Invite button
-        HBox boxTop = new HBox(5);
-
-        // Header
-        Text header = new Text("Header");
-        header.setFont(Font.font("Arial", FontWeight.BOLD , 20));
-        // Send Invite Button
+        // HBox eventBox with Event Name and Send Invite button
+        HBox eventBox = new HBox(5);
+        // Event Name (hardcoded)
+        Text eventName = new Text("Event Name");
+        eventName.setFont(h1);
+        // Send Invite button
         Button sendInviteButton = new Button("Send Invite");
-        // Add to boxTop
-        boxTop.getChildren().addAll(header, sendInviteButton);
+        // Add to eventBox
+        eventBox.getChildren().addAll(eventName, sendInviteButton);
 
-        layout.getChildren().add(boxTop);
+        // HBox participantsBox with Participants text, Edit and Add Participants button
+        HBox participantsBox = new HBox(5);
+        // Participants text
+        Text participants = new Text("Participants");
+        participants.setFont(h2);
+        // Edit Participant button
+        Button editParticipantButton = new Button("Edit");
+        // Add Participant button
+        Button addParticipantButton = new Button("Add");
+        // Add to participantBox
+        participantsBox.getChildren().addAll(participants, editParticipantButton, addParticipantButton);
+
+        // Names of the participants (hardcoded)
+        Text personNames = new Text("Chris, John, Anna, David");
+
+        layout.getChildren().addAll(eventBox, participantsBox, personNames);
 
         // Scene
         scene = new Scene(layout, 400, 400);
