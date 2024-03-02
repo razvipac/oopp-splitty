7c2583016b7b750abe68f1d92fc6ea2b174f2b2b
Mihai Rares Bobeica <M.R.Bobeica-1@student.tudelft.nl>
2024-03-02 13:39:56 +0100

    create the user preference class in order to use it for future LanguageManager to get and retrieve information

--- client/src/main/java/client/UserPreferences.java
+++ client/src/main/java/client/UserPreferences.java
@@ -1,2 +1,21 @@
-package client;public class UserPreferences {
+package client;
+
+import java.util.Locale;
+
+public class UserPreferences {
+    //in case of future preferences, add them here
+    private Locale preferredLanguage;
+
+    public UserPreferences(final Locale preferredLanguage) {
+        this.preferredLanguage = preferredLanguage;
+    }
+    public UserPreferences(){}
+
+    public Locale getPreferredLanguage() {
+        return this.preferredLanguage;
+    }
+
+    public void setPreferredLanguage(final Locale preferredLanguage) {
+        this.preferredLanguage = preferredLanguage;
+    }
 }
