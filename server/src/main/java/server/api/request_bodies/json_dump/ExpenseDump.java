4b5dd05a9939b1cfb21cabdf2fdb5782ede18cd9
Stanisław Malinowski <s.p.malinowski@student.tudelft.nl>
2024-03-06 21:40:10 +0100

    Add static methods to quickly transform entity objects into response format POJOs

--- server/src/main/java/server/api/request_bodies/json_dump/ExpenseDump.java
+++ server/src/main/java/server/api/request_bodies/json_dump/ExpenseDump.java
@@ -1,8 +1,19 @@
 package server.api.request_bodies.json_dump;
 
+import commons.Expense;
+
 public record ExpenseDump (
        Long id,
        String paidBy,
        Integer price,
        String item
-) { }
+) {
+    public static ExpenseDump build(Expense expense){
+        return new ExpenseDump(
+                expense.getId(),
+                expense.getPaidBy().getName(),
+                expense.getPrice(),
+                expense.getItem()
+        );
+    }
+}
--- server/src/main/java/server/api/request_bodies/json_dump/ParticipantDump.java
+++ server/src/main/java/server/api/request_bodies/json_dump/ParticipantDump.java
@@ -1,8 +1,19 @@
 package server.api.request_bodies.json_dump;
 
+import commons.Participant;
+
 public record ParticipantDump (
         String name,
         String email,
         String iban,
         String bic
-) { }
+) {
+    public static ParticipantDump build(Participant participant){
+        return new ParticipantDump(
+                participant.getName(),
+                participant.getEmail(),
+                participant.getIban(),
+                participant.getBic()
+        );
+    }
+}
