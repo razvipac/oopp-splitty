93b545b0720d25981bdfc72642feb31f837906a8
Naman Choudhary <namanchoudhary@student.tudelft.nl>
2024-03-07 16:46:06 +0100

    Added the AddEditExpense button to the temporarily home screen

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -101,7 +101,7 @@ public class Main extends Application {
         layout.setPadding(new Insets(10));
         // adding elements to layout
         layout.getChildren().addAll(startButton, eoButton, cdButton, invitationButton,
-                odButton,languageButton);
+                odButton,languageButton, addExpenseButton);
 
         // Scene
         mainScene = new Scene(layout, 400, 300); // changed this line
