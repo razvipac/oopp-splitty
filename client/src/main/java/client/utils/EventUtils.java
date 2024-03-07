cc5af42521a56e66e33e1c99462288da19ab9557
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-03-07 21:38:14 +0100

    Add EventUtils with addEvent method, change client build.gradle to allow for Java LocalDateTime

--- client/src/main/java/client/utils/EventUtils.java
+++ client/src/main/java/client/utils/EventUtils.java
@@ -12,15 +12,14 @@ public class EventUtils {
 
     private static final String SERVER = "http://localhost:8080/";
 
-    public Event addEvent(Event event) {
-        String eventName = event.getName();
+    public Event addEvent(String eventName) {
 
         return ClientBuilder.newClient(new ClientConfig())
                 .target(SERVER).path("api/v1/")
                 .queryParam("name", eventName)
                 .request(APPLICATION_JSON)
                 .accept(APPLICATION_JSON)
-                .post(Entity.entity(event, APPLICATION_JSON), Event.class);
+                .post(Entity.entity(eventName, APPLICATION_JSON), Event.class);
     }
 
 }
