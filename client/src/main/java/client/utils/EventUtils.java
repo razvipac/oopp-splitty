e8a83c0e48cb6eb99499f723bb52fc33fcdcc839
Naman Choudhary <namanchoudhary@student.tudelft.nl>
2024-03-17 14:12:11 +0100

    Fixed some Intellij errors

--- client/src/main/java/client/utils/EventUtils.java
+++ client/src/main/java/client/utils/EventUtils.java
@@ -16,10 +16,10 @@ public class EventUtils {
 
     private static final String SERVER = "http://localhost:8080/";
 
-    /**
-     * Gets all events.
-     * @return All events as a List
-     */
+//    /**
+//     * Gets all events.
+//     * @return All events as a List
+//     */
 //    public List<Event> getAllEvents() {
 //       return ClientBuilder.newClient(new ClientConfig())
 //               .target(SERVER).path("api/v1/")
@@ -54,7 +54,7 @@ public class EventUtils {
                 .target(SERVER).path("api/v1/" + code + "/expense")
                 .request(APPLICATION_JSON)
                 .accept(APPLICATION_JSON)
-                .get(new GenericType<List<Participant>>() {
+                .get(new GenericType<>() {
                 });
     }
 
