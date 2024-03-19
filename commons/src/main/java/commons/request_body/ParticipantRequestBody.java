2fda30545bc6d81e5a8d5805d45fbf725700d103
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-03-19 22:07:24 +0100

    Create copy of ParticipantRequestBody in commons, so that it can be used by the client

---
+++ commons/src/main/java/commons/request_body/ParticipantRequestBody.java
@@ -0,0 +1,29 @@
+package commons.request_body;
+
+import commons.Participant;
+
+/**
+ * The structure of the request body for the Participant object
+ *
+ * @param name name of the participant
+ * @param email email of the participant
+ * @param iban iban number of the participant
+ * @param bic bic number of the participant
+ */
+public record ParticipantRequestBody(
+        String name,
+        String email,
+        String iban,
+        String bic
+) {
+
+    public ParticipantRequestBody build(Participant p) {
+        return new ParticipantRequestBody(
+                p.getName(),
+                p.getEmail(),
+                p.getIban(),
+                p.getBic()
+        );
+    }
+
+}
