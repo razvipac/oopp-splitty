20258af3c493aeb309ff2fd1ceef2371cc9e8f1e
Mihai Rares Bobeica <M.R.Bobeica-1@student.tudelft.nl>
2024-03-20 20:11:14 +0100

    added test for LanguageManager

--- client/src/test/java/client/LanguageManagerTest.java
+++ client/src/test/java/client/LanguageManagerTest.java
@@ -1,18 +1,33 @@
 package client;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class LanguageManagerTest {
 
     @Test
     void loadSaveLanguage() {
+        LanguageManager languageManager = new LanguageManager(
+                "client/src/test/java/client/testFile.json");
+        languageManager.saveLanguage(new LanguageOption());
+        LanguageOption languageOption= languageManager.loadLanguage();
+        assertEquals(LanguageOption.Language.ENGLISH, languageOption.getLanguage());
 
     }
 
     @Test
     void get() {
-    }
+        LanguageManager languageManager = new LanguageManager(
+                "client/src/test/java/client/testFile.json");
+        LanguageOption languageOptionEnglish = new LanguageOption(LanguageOption.Language.ENGLISH);
+        LanguageOption languageOptionDutch = new LanguageOption(LanguageOption.Language.DUTCH);
 
-    @Test
-    void testGet() {
+        assertEquals("English",languageManager.get(languageOptionEnglish,"English"));
+        assertEquals("Dutch",languageManager.get(languageOptionEnglish,"Dutch"));
+        assertEquals("Engels",languageManager.get(languageOptionDutch,"English"));
+        assertEquals("Nederlands",languageManager.get(languageOptionDutch,"Dutch"));
+
+
+
     }
 }
