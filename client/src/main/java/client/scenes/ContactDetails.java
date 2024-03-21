13486257051b89ac75e23267f47ddf34a5c5c458
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-03-21 22:43:08 +0100

    Clean up ContactDetails and resize the TextBoxes

--- client/src/main/java/client/scenes/ContactDetails.java
+++ client/src/main/java/client/scenes/ContactDetails.java
@@ -6,11 +6,11 @@ import commons.Event;
 import commons.Participant;
 
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -25,6 +25,12 @@ public class ContactDetails {
     private final Event event;
 
     private final ServerUtils server = ServerUtils.getServerUtils();
+
+    private final GridPane gridPane = new GridPane();
+    private final TextField boxName = new TextField();
+    private final TextField boxEmail = new TextField();
+    private final TextField boxIban = new TextField();
+    private final TextField boxBic = new TextField();
 
     /**
      * Constructor for the contact details that calls the method to create the scene
@@ -51,119 +57,61 @@ public class ContactDetails {
     }
 
     /**
-     * create a GUI for the contact details
+     * Create a GUI for the contact details
      */
     public void createSceneContactDetails() {
         Text title = new Text("Add/Edit Participant");
         title.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
-        // Create a label
-        Label nameLabel = new Label("Name:");
-        // Create a text field
-        TextArea boxName = createTextArea("Enter the name here", 350, 100);
+        gridPane.setVgap(5);
+        gridPane.setHgap(10);
 
-        // Create an HBox to hold the label and text field
-        HBox hBoxName = createHBox(nameLabel, boxName, 10, 10);
-        hBoxName.setAlignment(Pos.CENTER);
-
-        Label emailLabel = new Label("Email:");
-        // Create a text field
-        TextArea boxEmail = createTextArea("Enter the email here", 350, 100);
-
-        // Create an HBox to hold the label and text field
-        HBox hBoxEmail = createHBox(emailLabel, boxEmail, 10, 10);
-        hBoxEmail.setAlignment(Pos.CENTER);
-
-        Label ibanLabel = new Label("IBAN:");
-        // Create a text field
-        TextArea boxIBAN = createTextArea("Enter the IBAN here", 350, 100);
-
-        // Create an HBox to hold the label and text field
-        HBox hBoxIban = new HBox(10); // 10 is the spacing between elements
-        hBoxIban.setPadding(new Insets(10)); // Padding around the HBox
-        hBoxIban.getChildren().addAll(ibanLabel, boxIBAN);
-        hBoxIban.setAlignment(Pos.CENTER);
-
-        Label bicLabel = new Label("BIC:");
-        // Create a text field
-        TextArea boxBIC = createTextArea("Enter the BIC here", 350, 100);
-
-        // Create an HBox to hold the label and text field
-        HBox hBoxBic = createHBox(bicLabel, boxBIC, 10, 10);
+        addToGridPane(boxName, "Name:", "Enter your name here", 200, 0);
+        addToGridPane(boxEmail, "Email:", "Enter your email here", 200, 1);
+        addToGridPane(boxIban, "IBAN:", "Enter your IBAN here", 200, 2);
+        addToGridPane(boxBic, "BIC:", "Enter your BIC here", 200, 3);
 
         // Adding buttons
-
         Button abort = new Button("Abort");
         abort.setOnAction(e -> {
             // return back
         });
 
-        // non functional
         Button ok = new Button("Ok");
         ok.setOnAction(e -> {
             Participant p = new Participant(boxName.getText(), event, boxEmail.getText(),
-                    boxIBAN.getText(), boxBIC.getText());
-            System.out.println(p.toString());
+                    boxName.getText(), boxBic.getText());
+            System.out.println(p);
             server.getParticipantUtils().addParticipant(p);
         });
 
-        Button backButton = new Button("Back");
-        backButton.setOnAction(e -> main.getPrimaryStage().setScene(main.getMainScene()));
-
         HBox hBoxButtons = new HBox(10); // 10 is the spacing between elements
-        hBoxBic.setPadding(new Insets(10)); // Padding around the HBox
-        hBoxBic.getChildren().addAll(abort, ok, backButton);
-        hBoxButtons.setAlignment(Pos.CENTER);
+        hBoxButtons.getChildren().addAll(abort, ok);
 
         // create the layout
-        VBox layout = new VBox();
-        layout.setSpacing(10);
-        layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().add(title);
+        VBox layout = new VBox(20);
+        layout.setPadding(new Insets(20));
+        layout.getChildren().addAll(title, gridPane, hBoxButtons);
 
-        layout.getChildren().addAll(hBoxName, hBoxEmail, hBoxIban, hBoxBic);
-        // the button is placed alongside the rest, not below, it needs fixing
-        layout.getChildren().addAll(hBoxButtons);
-
-        layout.setAlignment(Pos.CENTER);
-
-        scene = new Scene(layout, 555, 555);
-
-        layout.prefWidthProperty().bind(scene.widthProperty());
-        layout.prefHeightProperty().bind(scene.heightProperty());
+        scene = new Scene(layout, 300, 250);
     }
 
     /**
-     * Create an HBox to hold the label and text area
-     * @param label label to be added to the HBox
-     * @param textArea text area to be added to the HBox
-     * @param spacing spacing between elements
-     * @param padding padding around the HBox
-     * @return the HBox
+     * Adds the given TextField to gridPane at the given coordinates, along with a label.
+     * @param textField The (empty) TextField object
+     * @param labelText The String of the Label
+     * @param promptText The prompt text of the TextField
+     * @param prefWidth The preferred width of the TextField
+     * @param gridY The y coordinate of gridPane to add to.
      */
-    public HBox createHBox(Label label, TextArea textArea, int spacing, int padding) {
-        // Create an HBox to hold the label and text area
-        HBox hbox = new HBox(spacing);
-        hbox.setPadding(new Insets(padding));
-        hbox.getChildren().addAll(label, textArea);
+    public void addToGridPane(TextField textField, String labelText,
+                              String promptText, int prefWidth, int gridY) {
+        Label label = new Label(labelText);
+        textField.setPromptText(promptText);
+        textField.setPrefWidth(prefWidth);
 
-        return hbox;
-    }
-
-    /**
-     * Create a text area with the given parameters
-     * @param promptText Prompt text of the text area
-     * @param maxWidth Maximum width of the text area
-     * @param maxHeight Maximum height of the text area
-     * @return the text area
-     */
-    public TextArea createTextArea(String promptText, int maxWidth, int maxHeight) {
-        TextArea textArea = new TextArea();
-        textArea.setPromptText(promptText);
-        textArea.setMaxWidth(maxWidth);
-        textArea.setMaxHeight(maxHeight);
-
-        return textArea;
+        gridPane.add(label, 0, gridY);
+        gridPane.add(textField, 1, gridY);
     }
 
     /**
