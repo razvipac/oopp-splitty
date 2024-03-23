dbeb2a80a8e9a8e720c48468a9b7d71db34ae6a6
Razvan Paraschiv <r.n.paraschiv@student.tudelft.nl>
2024-03-23 13:13:46 +0100

    Resize the windows of the pages and make Back buttons work for Event Overview, Contact Details and Invitation

--- client/src/main/java/client/scenes/ContactDetails.java
+++ client/src/main/java/client/scenes/ContactDetails.java
@@ -46,8 +46,14 @@ public class ContactDetails {
     private void createSceneNoEvent() {
         VBox layout = new VBox(5);
         Text noEventText = new Text("No event found");
-        layout.getChildren().add(noEventText);
-        scene = new Scene(layout, 350, 380);
+        Button backButton = new Button("Back");
+        backButton.setOnAction(e -> main.getPrimaryStage().setScene(main.getMainScene()));
+
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(noEventText, backButton);
+        layout.setPadding(new Insets(20));
+
+        scene = new Scene(layout, 340, 280);
     }
 
     /**
