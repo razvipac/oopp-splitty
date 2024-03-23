3cf8c4f4263fbc62b3ebc7417d26bdf60861efce
Naman Choudhary <namanchoudhary@student.tudelft.nl>
2024-03-23 17:28:42 +0100

    Added extra tests to ParticipantTest

--- commons/src/test/java/commons/ParticipantTest.java
+++ commons/src/test/java/commons/ParticipantTest.java
@@ -1,18 +1,25 @@
-// CHECKSTYLE:OFF
 package commons;
+
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class ParticipantTest {
+    Event event = new Event(
+            "test",
+            "codecode",
+            LocalDateTime.of(1900, 1, 1, 0, 0, 0)
+    );
+
+    ParticipantId participantId1 = new ParticipantId("A", event);
+    ParticipantId participantId2 = new ParticipantId("A", event);
+    ParticipantId participantId3 = new ParticipantId("V", event);
+
     Participant p1 = new Participant(
             "A",
-            new Event(
-                    "test",
-                    "codecode",
-                    LocalDateTime.of(1900, 1, 1, 0, 0, 0)
-                    ),
+            event,
             "a@mail.com",
             "1234",
             "1234"
@@ -20,22 +27,15 @@ class ParticipantTest {
 
     Participant p2 = new Participant(
             "A",
-            new Event(
-                    "test",
-                    "codecode",
-                    LocalDateTime.of(1900, 1, 1, 0, 0, 0)
-            ),
+            event,
             "a@mail.com",
             "1234",
             "1234"
     );
+
     Participant p3 = new Participant(
             "V",
-            new Event(
-                    "test",
-                    "codecode",
-                    LocalDateTime.of(1900, 1, 1, 0, 0, 0)
-            ),
+            event,
             "a@mail.com",
             "1234",
             "1234"
@@ -43,27 +43,73 @@ class ParticipantTest {
 
     @Test
     void getName() {
-        assertEquals(p1.getName(), p2.getName());
-        assertNotEquals(p1.getName(), p3.getName());
+        assertEquals("A", p1.getName());
     }
+
     @Test
     void setName() {
-        assertNotEquals(p1, p3);
-        p3.setName("A");
-        p1.setEvent(p3.getEvent());
-        assertEquals(p1, p3);
+        p1.setName("NewName");
+        assertEquals("NewName", p1.getName());
     }
+
+    @Test
+    void getEvent() {
+        assertEquals(event, p1.getEvent());
+    }
+
+    @Test
+    void setEvent() {
+        Event newEvent = new Event(
+                "newTest",
+                "newCodecode",
+                LocalDateTime.of(2000, 1, 1, 0, 0, 0)
+        );
+        p1.setEvent(newEvent);
+        assertEquals(newEvent, p1.getEvent());
+    }
+
+    @Test
+    void getEmail() {
+        assertEquals("a@mail.com", p1.getEmail());
+    }
+
+    @Test
+    void setEmail() {
+        p1.setEmail("new@mail.com");
+        assertEquals("new@mail.com", p1.getEmail());
+    }
+
+    @Test
+    void getIban() {
+        assertEquals("1234", p1.getIban());
+    }
+
+    @Test
+    void setIban() {
+        p1.setIban("5678");
+        assertEquals("5678", p1.getIban());
+    }
+
+    @Test
+    void getBic() {
+        assertEquals("1234", p1.getBic());
+    }
+
+    @Test
+    void setBic() {
+        p1.setBic("5678");
+        assertEquals("5678", p1.getBic());
+    }
+
     @Test
     void testEquals() {
-        p1.setEvent(p2.getEvent());
-        assertEquals(p1, p2);
-        assertEquals(p1, p1);
-        assertNotEquals(p1, p3);
+        assertTrue(p1.equals(p2));
+        assertFalse(p1.equals(p3));
     }
 
     @Test
     void testHashCode() {
-        p1.setEvent(p2.getEvent());
         assertEquals(p1.hashCode(), p2.hashCode());
     }
+
 }
