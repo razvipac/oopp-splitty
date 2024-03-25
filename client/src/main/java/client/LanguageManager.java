8473a2e28d7255cb0be519fb8083e04dbd693c4c
Mihai Rares Bobeica <M.R.Bobeica-1@student.tudelft.nl>
2024-03-25 18:27:48 +0100

    now the flag images display properly

--- client/src/main/java/client/LanguageManager.java
+++ client/src/main/java/client/LanguageManager.java
@@ -3,6 +3,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.*;
 
@@ -126,8 +127,21 @@ public class LanguageManager {
      * @param key takes a key
      * @return the associated value with it in the json config file
      */
-    public String get(String key){
-        return this.get(LanguageButton.getCurrentLanguage(),key);
+    public String get(String key) {
+        return this.get(LanguageButton.getCurrentLanguage(), key);
     }
 
+    /**
+     * @param currentLanguage is a LanguageOption for which we want to return
+     *                        a proper ImageView of its flag
+     * @return a proper ImageView that looks like the language input flag.
+     */
+    public static ImageView createFlagIcon(LanguageOption currentLanguage) {
+        //getFlagImage currently returns null
+        Image flagImage = LanguageManager.getFlagImage(currentLanguage);
+        ImageView imageView = new ImageView(flagImage);
+        imageView.setFitWidth(20);
+        imageView.setFitHeight(15);
+        return imageView;
+    }
 }
