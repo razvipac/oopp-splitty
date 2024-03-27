551898ad0e3c2504ecbb0f3f5dfe21ccd6a2662f
Mihai Rares Bobeica <M.R.Bobeica-1@student.tudelft.nl>
2024-03-27 18:06:28 +0100

    add test for languageOption class

--- client/src/test/java/client/LanguageOptionTest.java
+++ client/src/test/java/client/LanguageOptionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LanguageOptionTest {
-    
+
     @Test
     void testToStringDutch() {
         LanguageOption languageOption = new LanguageOption(LanguageOption.Language.DUTCH);
