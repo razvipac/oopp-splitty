6400af8b18d3f211dc21df28835433280b11a949
Mihai Rares Bobeica <M.R.Bobeica-1@student.tudelft.nl>
2024-03-27 18:03:15 +0100

    test the LanguageOption test to about 95% line coverage

--- client/src/test/java/client/LanguageOptionTest.java
+++ client/src/test/java/client/LanguageOptionTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LanguageOptionTest {
+    
     @Test
     void testToStringDutch() {
         LanguageOption languageOption = new LanguageOption(LanguageOption.Language.DUTCH);
         assertEquals("Dutch", languageOption.toString());
 
     }
+    @Test
     void testToStringEnglish(){
         LanguageOption languageOption = new LanguageOption(LanguageOption.Language.ENGLISH);
         assertEquals("English", languageOption.toString());
@@ -19,6 +21,24 @@ class LanguageOptionTest {
     void getLanguage() {
         LanguageOption languageOption = new LanguageOption(LanguageOption.Language.DUTCH);
         assertEquals(LanguageOption.Language.DUTCH, languageOption.getLanguage());
+    }
 
+    @Test
+    void testEquals(){
+        LanguageOption l1 = new LanguageOption(LanguageOption.Language.DUTCH);
+        LanguageOption l2 = new LanguageOption(LanguageOption.Language.DUTCH);
+        assertEquals(l1, l2);
+    }
+    @Test
+    void testNotEquals(){
+        LanguageOption l1 = new LanguageOption(LanguageOption.Language.DUTCH);
+        LanguageOption l2 = new LanguageOption(LanguageOption.Language.ENGLISH);
+        assertNotEquals(l1, l2);
+    }
+    @Test
+    void testHash(){
+        LanguageOption l1 = new LanguageOption(LanguageOption.Language.DUTCH);
+        LanguageOption l2 = new LanguageOption(LanguageOption.Language.DUTCH);
+        assertEquals(l1.hashCode(),l2.hashCode());
     }
 }
