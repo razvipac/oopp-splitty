f2730130e36e923ab3fd32669c18767bbc50f2ca
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-03-27 20:26:49 +0100

    Create ParticipantDTO

---
+++ commons/src/main/java/commons/dto/ParticipantDTO.java
@@ -0,0 +1,49 @@
+package commons.dto;
+
+public class ParticipantDTO {
+
+    private String name;
+    private String email;
+    private String iban;
+    private String bic;
+
+    public ParticipantDTO(String name, String email, String iban, String bic) {
+        this.name = name;
+        this.email = email;
+        this.iban = iban;
+        this.bic = bic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getBic() {
+        return bic;
+    }
+
+    public void setBic(String bic) {
+        this.bic = bic;
+    }
+
+}
