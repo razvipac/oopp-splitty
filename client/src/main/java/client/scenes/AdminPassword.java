6b60a74320671a4a03846b8248050fa2d0d61dcd
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-03-31 15:44:13 +0200

    Create GUI page AdminPassword

---
+++ client/src/main/java/client/scenes/AdminPassword.java
@@ -0,0 +1,84 @@
+package client.scenes;
+
+import client.interfaces.DataBasedPopupController;
+import client.interfaces.StaticPopupController;
+import client.utils.ServerUtils;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class AdminPassword implements StaticPopupController {
+
+    private final MainCtrl mainCtrl;
+    private final ServerUtils serverUtils;
+    private final Stage window;
+    private Scene scene;
+    private boolean isOpen;
+
+    private boolean passwordIsCorrect;
+
+    public AdminPassword(MainCtrl mainCtrl, ServerUtils serverUtils) {
+        this.mainCtrl = mainCtrl;
+        this.serverUtils = serverUtils;
+        window = new Stage();
+        window.setTitle("Enter Admin Password");
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.initOwner(mainCtrl.getPrimaryStage());
+        window.setOnCloseRequest(e -> closeAlertBox());
+    }
+
+    public Scene initialize() {
+        // Password label
+        Label passwordLabel = new Label("Please enter the admin password:");
+
+        // Password field
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Enter password");
+
+        // Submit Button
+        Button submitButton = new Button("Submit");
+        submitButton.setOnAction(e -> {
+            String password = passwordField.getText();
+        });
+
+        // Initialize layout
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(10));
+        layout.getChildren().addAll(passwordLabel, passwordField, submitButton);
+
+        // Initialize scene
+        scene = new Scene(layout, 300, 120);
+        return scene;
+    }
+
+    /**
+     * Displays a modal alert box for adding a participant.
+     */
+    public void displayAlertBox() {
+        isOpen = true;
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
+    /**
+     * Closes the modal alert box and clears up the input fields.
+     */
+    public void closeAlertBox() {
+        isOpen = false;
+        window.close();
+    }
+
+    /**
+     * Getter for isOpen, true - window is open - false otherwise
+     * @return value for isOpen
+     */
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+}
