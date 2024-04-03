ad91352876c2060ecc340a585c27634a937de86a
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-04-03 23:40:22 +0200

    Add events to gridpane

--- client/src/main/java/client/scenes/AdminCtrl.java
+++ client/src/main/java/client/scenes/AdminCtrl.java
@@ -1,14 +1,16 @@
 package client.scenes;
 
+import client.interfaces.VoidSceneController;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import commons.dto.EventDTO;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 import java.util.List;
 
-public class AdminCtrl {
+public class AdminCtrl implements VoidSceneController {
 
     private final ServerUtils serverUtils;
     private final MainCtrl mainCtrl;
@@ -34,8 +36,20 @@ public class AdminCtrl {
 
     public void refresh() {
         events = serverUtils.getAllEvents();
+        addEventsToEventGrid();
     }
 
+    public void addEventsToEventGrid() {
+        for (int i = 0; i < events.size(); i++) {
+            EventDTO e = events.get(i);
+            Button eventNameButton = new Button(e.getName());
+            Button deleteButton = new Button("Delete");
+            Button downloadButton = new Button("Download");
 
+            eventGrid.add(eventNameButton, 0, i);
+            eventGrid.add(deleteButton, 1, i);
+            eventGrid.add(downloadButton, 2, i);
+        }
+    }
 
 }
