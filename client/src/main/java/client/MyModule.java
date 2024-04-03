d52da8fb521e0971cc90204d487757c231de6d7b
Stanisław Malinowski <s.p.malinowski@student.tudelft.nl>
2024-04-03 16:59:15 +0200

    Refactor EventOverview.java to use FXML

--- client/src/main/java/client/MyModule.java
+++ client/src/main/java/client/MyModule.java
@@ -15,6 +15,7 @@
  */
 package client;
 
+import client.scenes.EventOverviewCtrl;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
@@ -28,5 +29,6 @@ public class MyModule implements Module {
     public void configure(Binder binder) {
         binder.bind(MainCtrl.class).in(Scopes.SINGLETON);
         binder.bind(StartScreenCtrl.class).in(Scopes.SINGLETON);
+        binder.bind(EventOverviewCtrl.class).in(Scopes.SINGLETON);
     }
 }
