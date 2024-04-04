c4eea14915e75aacfc59c66c9097718157ee2550
Naman Choudhary <namanchoudhary@student.tudelft.nl>
2024-04-04 21:31:27 +0200

    add javadoc

--- client/src/main/java/client/scenes/StartScreenCtrl.java
+++ client/src/main/java/client/scenes/StartScreenCtrl.java
@@ -28,12 +28,21 @@ public class StartScreenCtrl implements VoidSceneController {
     private Set<EventDTO> recentlyJoinedEvents = new LinkedHashSet<>();
     private List<EventDTO> events;
 
+    /**
+     * Constructor for the StartScreenCtrl class.
+     * @param server The global serverUtils singleton.
+     * @param mainCtrl The scene of the mainCtrl class.
+     */
     @Inject
     public StartScreenCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
         this.server = server;
     }
 
+    /**
+     * Initializes the controller.
+     * Sets up event listeners and refreshes the scene.
+     */
     public void initialize(){
         createEventTextField.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.ENTER) createEvent();
@@ -46,6 +55,10 @@ public class StartScreenCtrl implements VoidSceneController {
         refresh();
     }
 
+    /**
+     * Handles the "Join Event" button action.
+     * Retrieves the event with the provided code and navigates to its overview.
+     */
     @FXML
     private void joinEvent() {
         String code = joinEventTextField.getText();
@@ -59,6 +72,10 @@ public class StartScreenCtrl implements VoidSceneController {
         else System.out.println("Event with code: " + code + " doesn't exist");
     }
 
+    /**
+     * Handles the "Create Event" button action.
+     * Creates a new event with the provided name and navigates to its overview.
+     */
     @FXML
     private void createEvent() {
         String eventName = createEventTextField.getText();
@@ -68,6 +85,9 @@ public class StartScreenCtrl implements VoidSceneController {
         mainCtrl.showEventOverview(event);
     }
 
+    /**
+     * Updates the recent events view. Displays the last 4 events that the user has joined.
+     */
     private void updateRecentEvents() {
         recentViewedEvents.getChildren().clear();
         int amountOfEvents = 0;
@@ -90,12 +110,20 @@ public class StartScreenCtrl implements VoidSceneController {
         }
     }
 
+    /**
+     * Retrieves the event with the provided code.
+     * @param code The code of the event to retrieve.
+     * @return The event with the provided code.
+     */
     private Optional<EventDTO> getEvent(String code) {
         return events.stream()
                 .filter(event -> event.getCode().equals(code))
                 .findFirst();
     }
 
+    /**
+     * Refreshes the start screen by clearing text fields and updating event data.
+     */
     public void refresh(){
         createEventTextField.clear();
         joinEventTextField.clear();
