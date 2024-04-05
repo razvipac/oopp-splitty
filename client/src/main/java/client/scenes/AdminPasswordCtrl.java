a8b6c07716079d1f3d6e03d6424ffaab5bf536f2
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-04-05 19:43:09 +0200

    Add constructor

--- client/src/main/java/client/scenes/AdminPasswordCtrl.java
+++ client/src/main/java/client/scenes/AdminPasswordCtrl.java
@@ -1,8 +1,19 @@
 package client.scenes;
 
 import client.interfaces.VoidSceneController;
+import client.utils.ServerUtils;
 
 public class AdminPasswordCtrl implements VoidSceneController {
+
+    private final ServerUtils serverUtils;
+    private final MainCtrl mainCtrl;
+
+    private boolean passwordIsCorrect;
+
+    public AdminPasswordCtrl(ServerUtils serverUtils, MainCtrl mainCtrl) {
+        this.serverUtils = serverUtils;
+        this.mainCtrl = mainCtrl;
+    }
 
     public void initialize() {
 
