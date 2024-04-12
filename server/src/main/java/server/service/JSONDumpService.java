c7cfd11fc5d2957df37eb8348ad0b434dc6f7471
Amin Abid <A.Abid-1@student.tudelft.nl>
2024-04-12 23:09:52 +0200

    Throw exception if event code is null or empty

--- server/src/main/java/server/service/JSONDumpService.java
+++ server/src/main/java/server/service/JSONDumpService.java
@@ -185,6 +185,8 @@ public class JSONDumpService {
         try {
 
             EventDTO eventDTO = jsonDumpEventDTO.eventDTO();
+            if(eventDTO.code() == null || eventDTO.code().equals(""))
+                throw new ImproperDumpFormatException("Event code is invalid");
             eventRepository.save(eventDTOMapper.newEntity(eventDTO));
 
             for (ParticipantDTO participantDTO : jsonDumpEventDTO.participantDTOs()) {
