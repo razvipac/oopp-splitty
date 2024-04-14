17667eab8e1ee2ec4cf86219cb9316c13fa34dae
Stanisław Malinowski <s.p.malinowski@student.tudelft.nl>
2024-04-14 23:28:12 +0200

    Finish auto-filling for ContactDetailsCtrl Bye!

--- client/src/main/java/client/scenes/ContactDetailsCtrl.java
+++ client/src/main/java/client/scenes/ContactDetailsCtrl.java
@@ -116,20 +116,7 @@ public class ContactDetailsCtrl implements DataBasedSceneController<EventDTO> {
             }
         } );
 
-        comboBoxName.setOnAction(actionEvent -> {
-            String selection = comboBoxName.getSelectionModel().getSelectedItem();
-            if (selection != null && (currentView == View.EDIT || currentView == View.DELETE)) {
-                Optional<ParticipantDTO> found = participants.stream()
-                        .filter(participantDTO -> participantDTO.name().equals(selection))
-                        .findFirst();
-
-                if (found.isPresent()) {
-                    boxEmail.setText(found.get().email());
-                    boxIban.setText(found.get().iban());
-                    boxBic.setText(found.get().bic());
-                }
-            }
-        });
+        comboBoxName.setOnAction(actionEvent -> autoFillForm());
 
         // listen to toggleView changes
         toggleViewListen();
@@ -208,11 +195,13 @@ public class ContactDetailsCtrl implements DataBasedSceneController<EventDTO> {
                 }
                 else if(selectedText.equals("Edit") || selectedText.equals("Bewerk")) {
                     setView(View.EDIT);
+                    autoFillForm();
                 }
                 else if (selectedText.equals("Delete")
                         || selectedText.equals("Sterge")
                         || selectedText.equals("Verwijderen")) {
                     setView(View.DELETE);
+                    autoFillForm();
                 }
             }
         }));
@@ -498,4 +487,20 @@ public class ContactDetailsCtrl implements DataBasedSceneController<EventDTO> {
         labelBIC.setText("BIC");
         comboBoxName.setPromptText(lm.get("Choose..."));
     }
+
+    private void autoFillForm() {
+        String selection = comboBoxName.getSelectionModel().getSelectedItem();
+        if (selection != null && (currentView == View.EDIT || currentView == View.DELETE)) {
+            Optional<ParticipantDTO> found = participants.stream()
+                    .filter(participantDTO -> participantDTO.name().equals(selection))
+                    .findFirst();
+
+            if (found.isPresent()) {
+                boxEmail.setText(found.get().email());
+                boxIban.setText(found.get().iban());
+                boxBic.setText(found.get().bic());
+            }
+        }
+    }
+
 }
